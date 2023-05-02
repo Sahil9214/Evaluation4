f3ae375e4f1eef6665c59d259f02e164eef70e38
Sahil9214 <utkarshsinghal369@gmail.com>
2023-05-02 16:32:14 +0530

    change deploy try in catch

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,25 +2,25 @@ var jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
-
-  try {
-    if (token) {
-    
+  if (token) {
+    try {
       let decoded = jwt.verify(token, "eval");
-    
+  
       if (decoded) {
         (req.body.authorId = decoded.authorID),
           (req.body.authorName = decoded.authorName);
         next();
-      } else {
-        res.status(200).send({ msg: "Wrong person !!!!" });
+      } 
+      else{
+res.status(200).send("something is wrong")
       }
-    } else {
-      res.status(200).send({ msg: "Token is not valid" });
-    }
-  } catch (err) {
+    } 
+    
+  
+  catch (err) {
     res.status(400).send("You are not an authorized person");
   }
 };
+}
 
 module.exports = { auth };
